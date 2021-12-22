503eb36fbabe296899dda86834c0e25caac22966
May Elgarat <mayelgarat1@gmail.com>
2021-12-22 17:02:48 +0200

    save to storage

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,16 +1,43 @@
 export const locService = {
-    getLocs,
+  getLocs,
 };
 
+import { storageService } from "./localStorage.service.js";
+const STORAGE_KEY = "locsDB";
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
+  {
+    id: makeId(),
+    name: "Greatplace",
+    lat: 32.047104,
+    lng: 34.832384,
+    createAt: Date.now(),
+  },
+  {
+    id: makeId(),
+    name: "Neveragain",
+    lat: 32.047201,
+    lng: 34.832581,
+    createAt: Date.now(),
+  },
 ];
 
 function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs);
-        }, 2000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(locs);
+    }, 2000);
+  });
+}
+
+function saveLocsToStorage() {
+  storageService.save(STORAGE_KEY, locs);
+}
+function makeId(length = 6) {
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var txt = "";
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return txt;
 }
