c1fe4a0b2a96f8dab4e9fd29f73461c6583e11d7
Lee Segal <segalee17@gmail.com>
2021-12-22 17:03:57 +0200

    added save to storage

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,11 +1,27 @@
+import { storageService } from './localStorage.service.js';
 export const locService = {
     getLocs,
 };
-
-const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
+const KEY_STORAGE = 'locsDB';
+const locs = [{
+        id: makeRandId(),
+        name: 'Greatplace',
+        lat: 32.047104,
+        lng: 34.832384,
+        createdAt: Date.now(),
+    },
+    {
+        id: makeRandId(),
+        name: 'Neveragain',
+        lat: 32.047201,
+        lng: 34.832581,
+        createdAt: Date.now(),
+    },
 ];
+
+function saveLocsToStorage() {
+    storageService.save(KEY_STORAGE, locs);
+}
 
 function getLocs() {
     return new Promise((resolve, reject) => {
@@ -13,4 +29,14 @@ function getLocs() {
             resolve(locs);
         }, 2000);
     });
+}
+
+function makeRandId(length = 6) {
+    const possible =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var txt = '';
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return txt;
 }
