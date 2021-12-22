cb13d6700164a6a933908fc72075a34fa4cda114
Lee Segal <segalee17@gmail.com>
2021-12-22 16:38:47 +0200

    pan curr location

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,5 +1,6 @@
-import { locService } from "./services/loc.service.js";
-import { mapService } from "./services/map.service.js";
+import { locService } from './services/loc.service.js';
+import { mapService } from './services/map.service.js';
+// import { storageService } from './services/localStorage.service.js';
 
 window.onload = onInit;
 window.onAddMarker = onAddMarker;
@@ -8,81 +9,78 @@ window.onGetLocs = onGetLocs;
 window.onGetUserPos = onGetUserPos;
 
 function onInit() {
-  
-  mapService
-    .initMap()
-    .then(() => {
-    //   console.log("Map is ready");
-    })
-    .catch(() => console.log("Error: cannot init map"));
-  onPanMyLocation();
+    mapService
+        .initMap()
+        .then(() => {
+            console.log('Map is ready');
+        })
+        .catch(() => console.log('Error: cannot init map'));
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 }
 
-function onPanMyLocation() {
-    var map = mapService.initMap().then(res=> {
-console.log(' res.data.map',  res.data.map);
-    return   res.data.map
-    })
-  const gMap =  map.then(map =>{
-      console.log('map', map);
-      map.addListener("click", (mapsMouseEvent) => {
-        // var locName = gName;
-        // var locName = prompt('Enter Location Name');
-        // Close the current InfoWindow.
-        infoWindow.close();
-        // Create a new InfoWindow.
-        infoWindow = new google.maps.InfoWindow({
-          position: mapsMouseEvent.latLng,
-        });
-        var newPos =
-          (" latlang", JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2));
-    
-        infoWindow.setContent(
-          JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-        );
-        infoWindow.open(map);
-        openModal(newPos);
-      });
-      return map
-    })
+// function onPanMyLocation() {
+//     var map = mapService.initMap().then((res) => {
+//         console.log(' res.data.map', res.data.map);
+//         return res.data.map;
+//     });
+//     const gMap = map.then((map) => {
+//         console.log('map', map);
+//         map.addListener('click', (mapsMouseEvent) => {
+//             // var locName = gName;
+//             // var locName = prompt('Enter Location Name');
+//             // Close the current InfoWindow.
+//             infoWindow.close();
+//             // Create a new InfoWindow.
+//             infoWindow = new google.maps.InfoWindow({
+//                 position: mapsMouseEvent.latLng,
+//             });
+//             var newPos =
+//                 (' latlang', JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2));
 
-//   const map = mapService.getMap();
+//             infoWindow.setContent(
+//                 JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+//             );
+//             infoWindow.open(map);
+//             openModal(newPos);
+//         });
+//         return map;
+//     });
 
- 
-}
+//     //   const map = mapService.getMap();
+// }
 
 function onAddMarker() {
-  console.log("Adding a marker");
-  mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
+    console.log('Adding a marker');
+    mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
 }
 
 function onGetLocs() {
-  locService.getLocs().then((locs) => {
-    console.log("Locations:", locs);
-    document.querySelector(".locs").innerText = JSON.stringify(locs);
-  });
+    locService.getLocs().then((locs) => {
+        console.log('Locations:', locs);
+        document.querySelector('.locs').innerText = JSON.stringify(locs);
+    });
 }
 
 function onGetUserPos() {
-  getPosition()
-    .then((pos) => {
-      console.log("User position is:", pos.coords);
-      document.querySelector(
-        ".user-pos"
-      ).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`;
-    })
-    .catch((err) => {
-      console.log("err!!!", err);
-    });
+    getPosition()
+        .then((pos) => {
+            console.log('User position is:', pos.coords);
+            document.querySelector(
+                '.user-pos'
+            ).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`;
+        })
+        .catch((err) => {
+            console.log('err!!!', err);
+        });
 }
+
 function onPanTo() {
-  console.log("Panning the Map");
-  mapService.panTo(35.6895, 139.6917);
+    console.log('Panning the Map');
+    mapService.panTo(35.6895, 139.6917);
 }
--- js/services/map.service.js
+++ js/services/map.service.js
@@ -1,57 +1,119 @@
 export const mapService = {
-  initMap,
-  addMarker,
-  panTo,
-  getMap,
-  gMap,
+    initMap,
+    addMarker,
+    panTo,
+    getMap,
+    gMap,
 };
-
-var gMap;
+import { storageService } from './localStorage.service.js';
+// var gMap;
 var infoWindow;
+const STORAGE_KEY = 'gMapDB';
+var gMap = storageService.load(STORAGE_KEY) || {};
+window.gMap = gMap;
+
 function initMap(lat = 32.0749831, lng = 34.9120554) {
-  return _connectGoogleApi().then(() => {
-    // console.log('google available');
-    infoWindow = new google.maps.InfoWindow();
-    gMap = new google.maps.Map(document.querySelector("#map"), {
-      center: { lat, lng },
-      zoom: 15,
+    // if (Object.keys(gMap).length !== 0) {
+    //     log
+    //     console.log('retrieving from Cache');
+    //     return Promise.resolve(gMap);
+    // }
+    return _connectGoogleApi().then(() => {
+        infoWindow = new google.maps.InfoWindow();
+        gMap = new google.maps.Map(document.querySelector('#map'), {
+            center: { lat, lng },
+            zoom: 15,
+        });
+        panCurrLocation();
+        console.log('gMap.data.map:', gMap.data.map);
+        // storageService.save(STORAGE_KEY, gMap.data.map);
+        console.log('gMap.data.map:', gMap.data.map);
+
+        return gMap;
     });
-    return {
-      gMap,
-      infoWindow,
-    };
-  });
+}
+
+function panCurrLocation() {
+    infoWindow = new google.maps.InfoWindow();
+    // const locationButton = document.createElement('button');
+    // locationButton.innerHTML = `<i class="far fa-compass"></i>`;
+    // locationButton.classList.add('custom-map-control-button');
+    // gMap.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+    // locationButton.addEventListener('click', () => {
+    //     if (navigator.geolocation) {
+    //         navigator.geolocation.getCurrentPosition(
+    //             (position) => {
+    //                 const pos = {
+    //                     lat: position.coords.latitude,
+    //                     lng: position.coords.longitude,
+    //                 };
+    //                 infoWindow.setPosition(pos);
+    //                 infoWindow.setContent('Location found.');
+    //                 infoWindow.open(gMap);
+    //                 console.log('gMap:', gMap);
+    //                 console.log(' infoWindow.open(gMap):', infoWindow.open(gMap));
+
+    //                 gMap.setCenter(pos);
+    //             },
+    //             () => {
+    //                 handleLocationError(true, infoWindow, gMap.getCenter());
+    //             }
+    //         );
+    //     } else {
+    //         // Browser doesn't support Geolocation
+    //         handleLocationError(false, infoWindow, gMap.getCenter());
+    //     }
+    // });
+    gMap.addListener('click', (mapsMouseEvent) => {
+        // var locName = gName;
+        // var locName = prompt('Enter Location Name');
+        // Close the current InfoWindow.
+        infoWindow.close();
+        // Create a new InfoWindow.
+        infoWindow = new google.maps.InfoWindow({
+            position: mapsMouseEvent.latLng,
+        });
+        var newPos =
+            (' latlang', JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2));
+
+        infoWindow.setContent(
+            JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+        );
+        infoWindow.open(gMap);
+        // openModal(newPos);
+    });
 }
 
 function getMap() {
-  console.log("line11 gMap", gMap);
-  return gMap;
+    console.log('line11 gMap', gMap);
+    return gMap;
 }
+
 function addMarker(loc) {
-  var marker = new google.maps.Marker({
-    position: loc,
-    map: gMap,
-    title: "Hello World!",
-  });
-  return marker;
+    var marker = new google.maps.Marker({
+        position: loc,
+        map: gMap,
+        title: 'Hello World!',
+    });
+    return marker;
 }
 
 function panTo(lat, lng) {
-  var laLatLng = new google.maps.LatLng(lat, lng);
+    var laLatLng = new google.maps.LatLng(lat, lng);
 
-  gMap.panTo(laLatLng);
+    gMap.panTo(laLatLng);
 }
 
 function _connectGoogleApi() {
-  if (window.google) return Promise.resolve();
-  const API_KEY = "AIzaSyBT1I0vf81TipqrRQ6ZrCfBKK20g1iG5ms"; //DONE: Enter your API Key
-  var elGoogleApi = document.createElement("script");
-  elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
-  elGoogleApi.async = true;
-  document.body.append(elGoogleApi);
+    if (window.google) return Promise.resolve();
+    const API_KEY = 'AIzaSyBT1I0vf81TipqrRQ6ZrCfBKK20g1iG5ms'; //DONE: Enter your API Key
+    var elGoogleApi = document.createElement('script');
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+    elGoogleApi.async = true;
+    document.body.append(elGoogleApi);
 
-  return new Promise((resolve, reject) => {
-    elGoogleApi.onload = resolve;
-    elGoogleApi.onerror = () => reject("Google script failed to load");
-  });
+    return new Promise((resolve, reject) => {
+        elGoogleApi.onload = resolve;
+        elGoogleApi.onerror = () => reject('Google script failed to load');
+    });
 }
